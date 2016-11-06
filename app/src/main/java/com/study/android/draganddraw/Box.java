a9c5beef05a126863c0bba3fd6bfd18054aa9e23
strategist k <815034557@qq.com>
2016-11-06 14:46:13 +0800

    practice
    简单的绘制

---
+++ app/src/main/java/com/study/android/draganddraw/Box.java
@@ -0,0 +1,29 @@
+package com.study.android.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by Administrator on 2016/11/5.
+ */
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
